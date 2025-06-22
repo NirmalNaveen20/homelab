0c4f397b1e9c3131efef5e960aad858a669f4a6d
= <=>
2025-06-22 13:52:08 +0530

    Add log analytics workspace resource

--- terraform/azure/Lab2/main.tf
+++ terraform/azure/Lab2/main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "main" {
   name = "${var.application_name}-${var.environment_name}-rg"
   location = var.primary_location
 }
+
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "${var.application_name}-${var.environment_name}-log"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
