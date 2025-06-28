e72e0122273cfe77aee7a37cefca4114c6e91140
= <=>
2025-06-28 11:28:06 +0530

    Add diagostic setting to keyvault

--- terraform/azure/Lab3/main.tf
+++ terraform/azure/Lab3/main.tf
@@ -24,3 +24,23 @@ resource "azurerm_role_assignment" "terraform_user" {
   role_definition_name = "Key Vault Administrator"
   principal_id         = data.azurerm_client_config.current.object_id
 }
+
+data "azurerm_log_analytics_workspace" "observability" {
+  name                = "observability-dev-log"
+  resource_group_name = "observability-dev-rg"
+}
+
+resource "azurerm_monitor_diagnostic_setting" "main" {
+  name               = "${var.application_name}-${var.environment_name}-diag"
+  target_resource_id = azurerm_key_vault.main.id
+
+  log_analytics_workspace_id = data.azurerm_log_analytics_workspace.observability.id
+
+  enabled_log {
+    category = "AuditEvent"
+  }
+
+  enabled_metric {
+    category = "AllMetrics"
+  }
+}
