e4efa243db7bcffd0a4a15a732745c221eafdfaa
= <=>
2025-06-28 14:25:47 +0530

    Add vent and subnet resources

--- terraform/azure/Lab5/main.tf
+++ terraform/azure/Lab5/main.tf
@@ -7,19 +7,25 @@ resource "azurerm_virtual_network" "main" {
   name                = "${var.application_name}-${var.environment_name}-vnet"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  address_space       = ["10.40.0.0/22"]
+  address_space       = [var.base_address_space]
+}
+
+
+locals {
+  alpha_address_space = cidrsubnet(var.base_address_space, 2, 0)
+  beta_address_space = cidrsubnet(var.base_address_space, 2, 1)
 }
 
 resource "azurerm_subnet" "alpha" {
   name                 = "snet-alpha"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.40.0.0/24"]
+  address_prefixes     = [local.alpha_address_space]
 }
 
 resource "azurerm_subnet" "beta" {
   name                 = "snet-beta"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.40.1.0/24"]
+  address_prefixes     = [local.beta_address_space]
 }
