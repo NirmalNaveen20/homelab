42b0639cd604b5b39a5371a0d3a69ba70c92c1d4
= <=>
2025-06-28 11:05:34 +0530

    Add local runner with same resources in Lab3

---
+++ terraform/azure/Lab4/.debug-dev.sh
@@ -0,0 +1,23 @@
+# Set the subscription 
+export ARM_SUBSCRIPTION_ID=""
+
+# Set the application / environment 
+export TF_VAR_application_name="lab4"
+export TF_VAR_environment_name="dev"
+
+# Set the backend 
+export BACKEND_RESOURCE_GROUP="chocolate-dev-rg"
+export BACKEND_STORAGE_ACCOUNT="wahrub6ws2st"
+export BACKEND_STORAGE_CONTAINER="tfstate" 
+export BACKEND_KEY=$TF_VAR_application_name-$TF_VAR_environment_name
+
+# Run terraform 
+terraform init \
+    -backend-config="resource_group_name=${BACKEND_RESOURCE_GROUP}" \
+    -backend-config="storage_account_name=${BACKEND_STORAGE_ACCOUNT}" \
+    -backend-config="container_name=${BACKEND_STORAGE_CONTAINER}" \
+    -backend-config="key=${BACKEND_KEY}"
+
+terraform "$@"
+
+rm -rf .terraform
---
+++ terraform/azure/Lab4/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "main" {
+  name     = "${var.application_name}-${var.environment_name}-rg"
+  location = var.primary_location
+}
+resource "random_string" "suffix" {
+  length  = 5
+  upper   = false
+  special = false
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "main" {
+  name                = "${var.application_name}-${var.environment_name}-${random_string.suffix.result}-kv"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+
+  sku_name = "standard"
+}
+
+resource "azurerm_role_assignment" "terraform_user" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
