862afcf15658f754d58a27d3261fa8ed8baeb277
= <=>
2025-07-01 12:44:52 +0530

    Create VM resource

---
+++ terraform/azure/Lab6/terraform.tfvars
@@ -0,0 +1,2 @@
+primary_location = "westus2"
+subscription_id = "a49abe1f-f1b0-48a0-aab0-960e145b5abc"
---
+++ terraform/azure/Lab6/versions.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.34.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.6.3"
+    }
+  }
+#   backend "azurerm" {
+#     resource_group_name  = "chocolate-dev-rg"
+#     storage_account_name = "wahrub6ws2st"
+#     container_name       = "tfstate"
+#     key                  = "devops-dev"
+#   }
+}
+
+provider "azurerm" {
+  features {
+  }
+  subscription_id = var.subscription_id
+}
